352a60fb490537afe3df0b2199930fef166279ff
FilizGozet <filiz.gozet@hotmail.com>
2018-12-03 00:15:25 +0300

    3.12.2018

--- SoftPro2/RegisterPage.aspx.cs
+++ SoftPro2/RegisterPage.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,10 +12,25 @@ public partial class RegisterPage : System.Web.UI.Page
     {
 
     }
-    
-    //yeni kullanıcı olustur ve kayıt işlemi yapar
-    protected void btnsubmit_Click(object sender, EventArgs e)
+   
+
+
+    protected void CreateUserWizard1_CreatedUser(object sender, EventArgs e)
     {
+        Guid userFKey;
+
+        MembershipUser mu = Membership.GetUser(CreateUserWizard1.UserName);
+        userFKey = (Guid)mu.ProviderUserKey;
+
+        TextBox txtad = (TextBox)CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("UserName");
+        TextBox txtkimlik = (TextBox)CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("txtkimlik");
+        TextBox txtsoyad = (TextBox)CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("txtsoyad");
+        DropDownList ddlcinsiyet = (DropDownList)CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("ddlcinsiyet");
+        TextBox txtdogumtarihi = (TextBox)CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("txtdogumtarihi");
+        TextBox txtmail = (TextBox)CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("Email");
+        TextBox txttel = (TextBox)CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("txttel");
+        TextBox txtsifre = (TextBox)CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("Password");
+
         string tckimlik = txtkimlik.Text;
         string ad = txtad.Text;
         string soyad = txtsoyad.Text;
